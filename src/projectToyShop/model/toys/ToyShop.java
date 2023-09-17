23b8bcfa4dff5374d338b4a3a58ba83c732b2888
nagovitsin <nagovitsin-kgs@mail.ru>
2023-09-18 00:12:04 +0700

    Class ToyShop

--- src/projectToyShop/model/toys/ToyShop.java
+++ src/projectToyShop/model/toys/ToyShop.java
@@ -1,5 +1,34 @@
 package projectToyShop.model.toys;
 
+import java.util.PriorityQueue;
+
+import projectToyShop.model.goods.Toy;
+
+/**
+ * 2). Создаём класс ToyShop, который будет представлять собой магазин игрушек.
+ * У класса должно быть следующее поле: очередь игрушек (тип PriorityQueue).
+ */
+
 public class ToyShop {
+
+    private PriorityQueue<Toy> toys;
+
+    public ToyShop() {
+
+        toys = new PriorityQueue<>();
+
+    }
+
+    public void addToy(Toy toy) {
+
+        toys.add(toy);
+
+    }
+
+    public Toy getToy() {
+
+        return toys.poll();
+
+    }
 
 }
